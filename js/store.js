72189abcc660e604768ad23417956e87d385cff7
Diana Garcia <dianasgr12@gmail.com>
2026-09-07 13:46:12 -0600

    Se sube boceto de tarjetas para mostrar el producto varitas.

---
+++ js/store.js
@@ -0,0 +1,43 @@
+/* ================== Variables ================== */
+
+const listValiras = [
+    {
+        "name": "Michael Corner",
+        "wand": {
+            "wood": "",
+            "core": "",
+            "length": null
+        }
+    },
+    {
+        "name": "Filius Flitwick",
+        "wand": {
+            "wood": "",
+            "core": "",
+            "length": null
+        }
+    },
+    {
+        "name": "Zacharias Smith",
+        "wand": {
+            "wood": "",
+            "core": "",
+            "length": null,
+            "img": "assets/sauco.jpg"
+        }
+    }
+];
+
+console.log("listValiras ", listValiras);
+
+
+/* ================== Function ================== */
+function getCardsVaritas(listValiras) {
+
+    for (const varita in listValiras) {
+
+    }
+
+};
+
+
