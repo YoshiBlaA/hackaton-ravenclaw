c317f890140af25c3cb6e8f02056a47271ebe790
Diana Garcia <dianasgr12@gmail.com>
2026-09-07 15:43:45 -0600

    feat se suben camios para mostrar card e maner dinamica

--- js/store.js
+++ js/store.js
@@ -28,45 +28,69 @@ getPotions();
 getCharacters();
 
 
+
 /* ================== Variables varitas ================== */
-const formEl = document.getElementById("formAlbum");
-const listValiras = [
+let listValiras = [
     {
         "name": "Michael Corner",
-        "wand": {
-            "wood": "",
-            "core": "",
-            "length": null
-        }
+        "wood": "Manzano",
+        "core": "Cuerno de unicornio",
+        "lengthV": "assets/vatia1.jpg"
+
     },
     {
         "name": "Filius Flitwick",
-        "wand": {
-            "wood": "",
-            "core": "",
-            "length": null
-        }
+        "wood": "",
+        "core": "Pluma de Fenix",
+        "lengthV": "assets/vatia2.jpg"
     },
     {
         "name": "Zacharias Smith",
-        "wand": {
-            "wood": "",
-            "core": "",
-            "length": null,
-            "img": "assets/sauco.jpg"
-        }
+        "wood": "Sauce",
+        "core": "",
+        "lengthV": null,
+        "img": "assets/vatia3.jpg"
+
     }
 ];
+const cardsVaritas = document.getElementById("cardsVaritas");
 
 console.log("listValiras ", listValiras);
 
 
 /* ================== Function varitas ================== */
-function getCardsVaritas(listValiras) {
+const createCard = (varita, htmlElement) => {
+    const card = `<div class="col">
+                <div class="card cardVarita" style="max-width: 18rem;">
+                    <div class="card-header cardVaritaEncabezado">
+                        <h4> ${varita.name} </h4>
+                    </div>
+                    <div class="divImgFormat">
+                        <img src="${varita.img}" class="rounded float-start imgVarita" alt="...">
+                    </div>
 
-    for (const varita in listValiras) {
-
-    }
-
+                    <div class="card-body ">
+                        <h5 class="card-title">Descripcion:</h5>
+                        <p class="card-text">
+                        <ul>
+                            <li> Nucleo: ${varita.core}</li>
+                            <li> Madera:  ${varita.wood} </li>
+                            <li> Largo:  ${varita.lengthV} </li>
+                            <li> Precio: $ 32.00</li>
+                        </ul>
+                        </p>
+                    </div>
+                    <div class="card-footer cardVaritaPie">
+                        <button type="button" class="btn btn-warning btnFormat ">Comprar</button>
+                    </div>
+                </div>
+            </div>`;
+    htmlElement.insertAdjacentHTML("beforeend", card);
 };
 
+
+function getCardsVaritas(listValiras) {
+    listValiras.map((varita) => createCard(varita, cardsVaritas));
+};
+
+getCardsVaritas(listValiras);
